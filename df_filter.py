e1928713a3076e8062f063eac1ce665b50095c94
Michael Oyatsi <oyatsimichael@gmail.com>
2026-03-17 00:27:23 -0700

    Modify filter operations from pandas to ibis.

--- df_filter.py
+++ df_filter.py
@@ -1,25 +1,26 @@
 import pandas as pd
+import ibis
  
-def filter_data(df: pd.DataFrame, artist_input: str, platform_input: str) -> pd.DataFrame:
+def filter_data(df: ibis.expr.types.relations.Table, artist_input: str, platform_input: str) -> pd.DataFrame:
     """
     Filter a DataFrame by artist name and platform.
  
     Args:
-        df: The source DataFrame containing 'Artist' and 'most_playedon' columns.
+        df: An Ibis Table Expression that contains 'Artist' and 'most_playedon' columns.
         artist: Artist name to filter by. Given by input_selectize("artist", ...)
         platform: Platform to filter by. Given by input_radio_buttons where the choices are choices=["Spotify", "Youtube", "Both"]
  
     Returns:
         A filtered copy of the DataFrame to used as a reactive cal and for other components/outputs.
     """
-    artist = artist_input.strip()
+    artist = artist_input
  
     if artist:
-        filtered_df = df[df["Artist"].str.lower() == artist.lower()].copy()
+        filtered_df = df.filter([df.Artist == artist])
     else:
-        filtered_df = df.copy()
+        filtered_df = df
  
     if platform_input != "Both":
-        filtered_df = filtered_df[filtered_df["most_playedon"] == platform_input]
+        filtered_df = filtered_df.filter([filtered_df.most_playedon == platform_input])
  
-    return filtered_df
+    return filtered_df.to_pandas()
